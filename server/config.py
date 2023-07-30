7f8dc3567529ca38f67f802b2a4997d0b0f478a0
Kevin Salinas <kevinsalinas155@gmail.com>
2023-07-30 17:54:41 -0400

    install config dependencies

---
+++ server/config.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+
+
